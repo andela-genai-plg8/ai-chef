7f5c321a9ddce033858d9dfedb0f84b041c8de6e
Kolade Oluwagbenga Ige <kolade.ige@gmail.com>
2025-09-13 13:21:48 +0100

    feat: update environment configuration and dependencies; refine exports in index.ts for better structure

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
-export { findRecipe  } from "./controllers/findRecipe";
+export { findRecipe } from "./controllers/findRecipe";
 export { parseRecipe } from "./controllers/parseRecipe";
-export { bootstrap   } from "./controllers/bootstrap";
-export { chat        } from "./controllers/chat";
-export { tagRecipes, cleanRecipes } from "./jobs/tagRecipes";
+export { bootstrap } from "./controllers/bootstrap";
+export { chat } from "./controllers/chat";
+export { computeVector, scheduleComputeVector } from "./jobs/computeVector";
+// export { tagRecipes, cleanRecipes } from "./jobs/tagRecipes";
