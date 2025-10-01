6f1d933a00f22e59607035e462f92e1332dd236d
Kolade Oluwagbenga Ige <kolade.ige@gmail.com>
2025-10-01 16:45:10 +0100

    refactor: remove duplicate export of parseRecipe and ensure computeVector exports correctly

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,5 +2,4 @@ export { findRecipe   } from "./controllers/findRecipe";
 export { parseRecipe  } from "./controllers/parseRecipe";
 export { bootstrap    } from "./controllers/bootstrap";
 export { chat, speechToText } from "./controllers/chat";
-export { parseRecipe } from "./controllers/parseRecipe";
 export { computeVector, scheduleComputeVector } from "./jobs/computeVector";
