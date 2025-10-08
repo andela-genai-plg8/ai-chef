1ea8b4d3251722fe44f2d2538a9c8f2f2eee1a11
Kolade Oluwagbenga Ige <kolade.ige@gmail.com>
2025-10-08 23:29:36 +0100

    feat: export publishRecipe function from controllers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 export { findRecipe } from "./controllers/findRecipe";
 export { parseRecipe } from "./controllers/parseRecipe";
+export { publishRecipe } from "./controllers/publishRecipe";
 export { bootstrap } from "./controllers/bootstrap";
 export { chat, speechToText } from "./controllers/chat";
 export { computeVector, scheduleComputeVector } from "./jobs/computeVector";
